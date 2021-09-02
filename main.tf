2cd02d22447c0363005ceb3ba2c41a1d3961991d
Santiago Bertone <ado.bertone@gmail.com>
2021-09-02 16:03:46 -0300

    agregado de vpc al main

--- main.tf
+++ main.tf
@@ -4,3 +4,10 @@ provider "google" {
     region = "southamerica-east1"
     zone = "southamerica-east1-a"
 }
+
+resource "google_compute_network" "mfpvpc" {
+  name                    = "sanpablo1anet"
+  auto_create_subnetworks = false
+  ip_cidr_range = "192.168.169.0/24"
+  mtu                     = 1460
+}
