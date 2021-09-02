ade50d3ba320aec8920c92f1cef92b980d7a9cab
Santiago Bertone <ado.bertone@gmail.com>
2021-09-02 15:47:01 -0300

    add zone to main.tf

--- main.tf
+++ main.tf
@@ -2,4 +2,5 @@ provider "google" {
     credentials = "${file("spring-house-324817-6b1d00e5a152.json")}"
     project = "spring-house-324817"
     region = "southamerica-east1"
+    zone = "southamerica-east1-a"
 }
