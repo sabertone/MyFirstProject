57e41eb0fe9c01bb400f36be63c8ed829a39f5c5
Santiago Bertone <ado.bertone@gmail.com>
2021-09-09 10:03:50 -0300

    add vars and output

---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+project                  = "spring-house-324817"
+credentials_file         = "spring-house-324817-6b1d00e5a152.json"
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "project" { }
+
+variable "credentials_file" { }
+
+variable "region" {
+  default = "southamerica-east1"
+}
+
+variable "zone" {
+  default = "southamerica-east1-a"
+}
